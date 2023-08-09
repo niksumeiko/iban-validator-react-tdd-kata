48efaf6da3006595b6d7ea5992761ea55c5f2953
Nik Sumeiko <nik.sumeiko@gmail.com>
2023-08-09 07:56:07 +0200

    feat: 1 Design system

---
+++ src/design-system/components/FocusPageLayout.tsx
@@ -0,0 +1,12 @@
+import type { PropsWithChildren } from 'react';
+
+export const FocusPageLayout = ({ children }: PropsWithChildren) => {
+    return (
+        // eslint-disable-next-line internal/jsx-no-inline-styles
+        <div className="container mx-auto h-screen" style={{ maxWidth: '640px' }}>
+            <div className="pt-8 md:pt-16 h-full">
+                <div className="p-4 h-full bg-white rounded-t-md">{children}</div>
+            </div>
+        </div>
+    );
+};
