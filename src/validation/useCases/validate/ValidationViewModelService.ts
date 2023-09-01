71160269046f91dd04bfeecd1e47425e09dab59b
Nik Sumeiko <nik.sumeiko@gmail.com>
2023-09-01 18:09:32 +0200

    test/feat: View model retrieval > validation results

--- src/validation/useCases/validate/ValidationViewModelService.ts
+++ src/validation/useCases/validate/ValidationViewModelService.ts
@@ -1,7 +1,37 @@
 import type { ValidationResponse } from '../../api/ValidationApiService';
 
 export function createIbanValidationViewModel(validation?: ValidationResponse) {
+    const results = [];
+
+    if (validation) {
+        results.push('Valid IBAN');
+    }
+
+    if (
+        typeof validation?.bank?.trustScore === 'number' &&
+        validation?.bank?.trustScore > 7
+    ) {
+        results.push('Trusted bank');
+    }
+
+    if (validation?.flags.includes('INSTANT')) {
+        results.push('Accepts instant payments');
+    }
+
+    if (validation?.flags.includes('POSITIVE_HISTORY')) {
+        results.push('Positive operation history');
+    }
+
+    if (validation && !validation?.flags.includes('SECURITY_CLAIMS')) {
+        results.push('No security claims');
+    }
+
+    if (validation?.flags.includes('PSD2')) {
+        results.push('Complies with Payment Services Directive (PSD2)');
+    }
+
     return {
+        validationResults: results,
         isValidationAvailable: Boolean(validation),
     };
 }
--- src/validation/useCases/validate/__tests__/ValidationViewModelService.test.ts
+++ src/validation/useCases/validate/__tests__/ValidationViewModelService.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 
 import { createIbanValidationViewModel } from '../ValidationViewModelService';
+import type { ValidationResponse } from '../../../api/ValidationApiService';
 
 describe('validation view model retrieval', () => {
     describe('validation availability', () => {
@@ -13,11 +14,108 @@ describe('validation view model retrieval', () => {
         });
 
         it('returns availability when validation is available', () => {
-            const validation = { iban: 'x', flags: ['INSTANT'] };
+            const validation: ValidationResponse = { iban: 'x', flags: ['INSTANT'] };
 
             const result = createIbanValidationViewModel(validation);
 
             expect(result.isValidationAvailable).toBe(true);
+        });
+    });
+
+    describe('validation results', () => {
+        it('returns empty when validation is unavailable', () => {
+            const validation = undefined;
+
+            const result = createIbanValidationViewModel(validation);
+
+            expect(result.validationResults).toEqual([]);
+        });
+
+        it('returns results when validation is available', () => {
+            const validation: ValidationResponse = { iban: 'x', flags: [] };
+
+            const result = createIbanValidationViewModel(validation);
+
+            expect(result.validationResults).toContain('Valid IBAN');
+        });
+
+        it('returns results when iban belongs to a trusted bank', () => {
+            const validation: ValidationResponse = {
+                iban: 'x',
+                flags: [],
+                bank: {
+                    trustScore: 8,
+                },
+            };
+
+            const result = createIbanValidationViewModel(validation);
+
+            expect(result.validationResults).toContain('Trusted bank');
+        });
+
+        it('returns results when instant payments are accepted', () => {
+            const validation: ValidationResponse = {
+                iban: 'x',
+                flags: ['INSTANT'],
+            };
+
+            const result = createIbanValidationViewModel(validation);
+
+            expect(result.validationResults).toContain('Accepts instant payments');
+        });
+
+        it('returns results when iban has positive operations history', () => {
+            const validation: ValidationResponse = {
+                iban: 'x',
+                flags: ['POSITIVE_HISTORY'],
+            };
+
+            const result = createIbanValidationViewModel(validation);
+
+            expect(result.validationResults).toContain('Positive operation history');
+        });
+
+        it('returns results when iban has no security claims', () => {
+            const validation: ValidationResponse = {
+                iban: 'x',
+                flags: [],
+            };
+
+            const result = createIbanValidationViewModel(validation);
+
+            expect(result.validationResults).toContain('No security claims');
+        });
+
+        it('returns results when iban complies with PSD2', () => {
+            const validation: ValidationResponse = {
+                iban: 'x',
+                flags: ['PSD2'],
+            };
+
+            const result = createIbanValidationViewModel(validation);
+
+            expect(result.validationResults).toContain(
+                'Complies with Payment Services Directive (PSD2)',
+            );
+        });
+
+        it('returns all results for valid iban', () => {
+            const validation: ValidationResponse = {
+                iban: 'x',
+                flags: ['INSTANT', 'POSITIVE_HISTORY', 'PSD2'],
+                bank: { trustScore: 8 },
+            };
+
+            const result = createIbanValidationViewModel(validation);
+
+            expect(result.validationResults).toEqual([
+                'Valid IBAN',
+                'Trusted bank',
+                'Accepts instant payments',
+                'Positive operation history',
+                'No security claims',
+                'Complies with Payment Services Directive (PSD2)',
+            ]);
         });
     });
 });
