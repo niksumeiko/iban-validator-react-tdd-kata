91f52ab735760644839dd300a601aa3742ceb623
Adnen Rebai <adnen.rebai@gmail.com>
2023-11-07 23:35:24 +0100

    update : vite.config.ts to support custom env name

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+import dotenv from 'dotenv';
+dotenv.config();
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-    },
-    test: {
-        include: ['**/*.test.ts'],
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [react()],
+        server: {
+            port: 5173,
+        },
+        test: {
+            include: ['**/*.test.ts'],
+        },
+        define: {
+            'process.env.API_URL': JSON.stringify(env.API_URL),
+
+            // If you want to exposes all env variables, which is not recommended
+            // 'process.env': env
+        },
+    };
 });
